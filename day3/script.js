7e27373df85f30c37be2b0a0587295dc9ce5df52
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2020-07-17 18:41:22 +0200

    its not looking good right now but i have something, lets makes it prettier tomorrow

--- day3/script.js
+++ day3/script.js
@@ -1,8 +1,12 @@
+var output, input;
+
 function measurement() {
     if ($('#Measurement').val() == 1) {
         $('.unit-length').hide()
         $('.unit-temperature').hide()
         $('.unit-mass').show()
+
+
     }else if ($('#Measurement').val() == 2) {
         $('.unit-length').hide()
         $('.unit-mass').hide()
@@ -15,5 +19,49 @@ function measurement() {
         $('.unit-mass').hide()
         $('.unit-temperature').hide()
         $('.unit-length').hide()
+    }
+}
+
+
+
+
+
+function convert() {
+    if ($('#pickedunit-mass').val() == 1 && $('#convertedunit-mass').val() == 1) {
+        input = $('#convert-mass').val()
+        output = input 
+        console.log(output)
+    }else if($('#pickedunit-mass').val() == 2 && $('#convertedunit-mass').val() == 2) {
+        input = $('#convert-mass').val()
+        output = input 
+        console.log(output)
+    }else if ($('#pickedunit-mass').val() == 3 && $('#convertedunit-mass').val() == 3) {
+        input = $('#convert-mass').val()
+        input = output
+        console.log(output)
+    } else if ($('#pickedunut-mass').val() == 1 && ($('#convertedunit-mass').val() == 2)) {
+        input = $('#convert-mass').val()
+        output = input * 0.001
+        console.log(output)
+    }else if ($('#pickedunit-mass').val() == 1 && ($('#convertedunit-mass').val() == 3)) {
+        input = $('#convert-mass').val()
+        output = input * 0.000001
+        console.log(output)
+    }else if ($('#pickedunit-mass').val() == 2 && ($('#convertedunit-mass').val() == 1)) {
+        input = $('#convert-mass').val()
+        output = input * 1000
+        console.log(output)
+    }else if ($('#pickedunit-mass').val() == 2 && ($('#convertedunit-mass').val() == 3)) {
+        input = $('#convert-mass').val()
+        output = input / 1000
+        console.log(output)
+    }else if ($('#pickedunit-mass').val() == 3 && ($('#convertedunit-mass').val() == 1)) {
+        input = $('#convert-mass').val()
+        output = input * 1000000
+        console.log(output)
+    }else if($('#pickedunit-mass').val() == 3 && ($('#convertedunit-mass').val() == 2)) {
+        input = $('#convert-mass').val()
+        output = input * 1000
+        console.log(output)
     }
 }
